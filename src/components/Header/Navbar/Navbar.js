b35bb3f32488a362977641d8d52fbcc72b7ec53b
khaledtasmia <khaled.tasmia33@gmail.com>
2021-05-29 20:01:09 +0600

    new changes

--- src/components/Header/Navbar/Navbar.js
+++ src/components/Header/Navbar/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
             <nav class="navbar navbar-expand-lg navbar-light navy fixed-top">
                 <div class="container-fluid">
                     <img className="img-fluid" style={{ width: "80px" }} src={parkLogo} alt="" srcset="" />
-                    <a class="navbar-brand fs-2 ms-1" href="#header">Fun Park</a>
+                    <a class="navbar-brand fs-2 ms-1 option" href="#header">Fun Park</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
@@ -18,16 +18,16 @@ const Navbar = () => {
                                 <a class="nav-link active option" aria-current="page" href="#header">Home</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#about">About</a>
+                                <a class="nav-link active option" aria-current="page" href="#about">About</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#offers">Offers</a>
+                                <a class="nav-link active option" aria-current="page" href="#offers">Offers</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#benefits">Benefits</a>
+                                <a class="nav-link active option" aria-current="page" href="#benefits">Benefits</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#contactUs">Contact Us</a>
+                                <a class="nav-link active option" aria-current="page" href="#contactUs">Contact Us</a>
                             </li>
                         </ul>
                     </div>
