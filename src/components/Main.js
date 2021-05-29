3987dc04d8080ab208d31e6e51961e8f9bf88956
niloycj7 <niloyrahman31@gmail.com>
2021-05-29 22:39:28 +0600

    footer added

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import About from './About/About';
+import Footer from './Footer/Footer';
 // import Deals from './Deals/Deals';
 import Header from './Header/Header';
 
@@ -9,6 +10,7 @@ const Main = () => {
             <Header/>
             <About/>
             {/* <Deals/> */}
+            <Footer></Footer>
         </div>
     );
 };
