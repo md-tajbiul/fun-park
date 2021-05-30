58c420d46a5b392cecbcc7d8b91b1bcb02d56393
khaledtasmia <khaled.tasmia33@gmail.com>
2021-05-30 15:53:08 +0600

    login included

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDIxuhkAKS8D760Ns2TrHYfVrwcNTFQknU",
+    authDomain: "funpark-cdfe5.firebaseapp.com",
+    projectId: "funpark-cdfe5",
+    storageBucket: "funpark-cdfe5.appspot.com",
+    messagingSenderId: "493301546463",
+    appId: "1:493301546463:web:13c5f14efd272993da586a"
+  };
+
+  export default firebaseConfig;
